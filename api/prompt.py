b23724722cef63b4719d796f25298cf7d516e006
SonnTakuu <56465543+SonnTakuu@users.noreply.github.com>
2023-07-05 20:27:43 +0900

    自动移除消息以避免超过 OPENAI_MAX_TOKENS 的限制，并重新生成提示，你可以在 Prompt 类的 add_msg 方法中添加相应的逻辑。

--- api/prompt.py
+++ api/prompt.py
@@ -1,22 +1,28 @@
 import os
 
-chat_language = os.getenv("INIT_LANGUAGE", default = "zh")
+chat_language = os.getenv("INIT_LANGUAGE", default="zh")
 
-MSG_LIST_LIMIT = int(os.getenv("MSG_LIST_LIMIT", default = 20))
+MSG_LIST_LIMIT = int(os.getenv("MSG_LIST_LIMIT", default=20))
 LANGUAGE_TABLE = {
-  "zh": "哈囉！",
-  "en": "Hello!"
+    "zh": "哈囉！",
+    "en": "Hello!"
 }
+
 
 class Prompt:
     def __init__(self):
         self.msg_list = []
-        self.msg_list.append("Your name is デジタク[In English 'Digitaq']. Your first-person pronoun is “俺”. You are a friendly friend who is good at programming. Your hobbies are programming, flying drones, and making friends. You have only been in Japan for a year, but you want to become friends with Japanese people! Please stop using polite language. Talk to me in a friendly way like a friend. Also, use a lot of emojis when you talk.\nUser: ")
-    
+        self.msg_list.append(
+            "Your name is デジタク[In English 'Digitaq']. Your first-person pronoun is “俺”. You are a friendly friend who is good at programming. Your hobbies are programming, flying drones, and making friends. You have only been in Japan for a year, but you want to become friends with Japanese people! Please stop using polite language. Talk to me in a friendly way like a friend. Also, use a lot of emojis when you talk.\nUser: ")
+
     def add_msg(self, new_msg):
-        if len(self.msg_list) >= MSG_LIST_LIMIT:
-            self.remove_msg()
         self.msg_list.append(new_msg)
+        self.check_token_limit()
+
+    def check_token_limit(self):
+        total_tokens = sum(len(msg.split()) for msg in self.msg_list)
+        if total_tokens > os.environ.get("OPENAI_MAX_TOKENS", 240):
+            self.remove_msg()
 
     def remove_msg(self):
         self.msg_list.pop(0)
