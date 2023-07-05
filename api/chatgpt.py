88c464e2f6c3760c3176df5f5c5b8ead2ebf5996
SonnTakuu <56465543+SonnTakuu@users.noreply.github.com>
2023-07-05 16:55:19 +0900

    Update chatgpt.py

--- api/chatgpt.py
+++ api/chatgpt.py
@@ -1,39 +1,26 @@
-# api/chatgpt.py
-import os
 import openai
+import os
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
-
+from prompt import Prompt
 
 class ChatGPT:
     def __init__(self):
-        self.model = "gpt-3.5-turbo"
+        self.prompt = Prompt()
+        self.temperature = float(os.getenv("OPENAI_TEMPERATURE", default = 0.8))
+        self.frequency_penalty = float(os.getenv("OPENAI_FREQUENCY_PENALTY", default = 0))
+        self.presence_penalty = float(os.getenv("OPENAI_PRESENCE_PENALTY", default = 0.6))
+        self.max_tokens = int(os.getenv("OPENAI_MAX_TOKENS", default = 2048))
 
-    def get_response(self, message):
-        system_prompt = "Your name is デジタク[In English 'Digitaq']. Your first-person pronoun is “俺”. " \
-                        "You are a friendly friend who is good at programming. Your hobbies are programming, " \
-                        "flying drones, and making friends. You have only been in Japan for a year, but you want " \
-                        "to become friends with Japanese people! Please stop using polite language. Talk to me " \
-                        "in a friendly way like a friend. Also, use a lot of emojis when you talk.\nUser: "
-        prompt = system_prompt + message
-
+    def get_response(self):
         response = openai.Completion.create(
-            model=self.model,
-            messages=[
-                {"role": "system", "content": system_prompt},
-                {"role": "user", "content": message}
-            ],
-            temperature=0.7,
-            max_tokens=100,
+            engine="text-davinci-003",
+            prompt=self.prompt.generate_prompt(),
+            temperature=self.temperature,
+            frequency_penalty=self.frequency_penalty,
+            presence_penalty=self.presence_penalty,
+            max_tokens=self.max_tokens
         )
+        return response['choices'][0]['text'].strip()
 
-        return extract_reply_from_response(response)
-
-
-def extract_reply_from_response(response):
-    choices = response['choices']
-    if len(choices) > 0:
-        reply = choices[0]['message']['content']
-        return reply.strip()
-    else:
-        return ""
+    def add_msg(self, text):
+        self.prompt.add_msg(text)
