870f2325fb0db31f216de418dd35a0f5dd30f38e
SonnTakuu <56465543+SonnTakuu@users.noreply.github.com>
2023-07-05 16:40:38 +0900

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,4 +1,4 @@
-# old
+# index.py
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
@@ -9,7 +9,7 @@ import os
 
 line_bot_api = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
 line_handler = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET"))
-working_status = os.getenv("DEFALUT_TALKING", default = "true").lower() == "true"
+working_status = os.getenv("DEFALUT_TALKING", default="true").lower() == "true"
 
 app = Flask(__name__)
 chatgpt = ChatGPT()
@@ -55,46 +55,10 @@ def handle_message(event):
         return
 
     if working_status:
-        response = call_chatgpt_api(event.message.text)
-        reply_msg = extract_reply_from_response(response)
+        reply_msg = chatgpt.get_response(event.message.text)
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text=reply_msg))
-
-def call_chatgpt_api(message):
-    import os
-    import openai
-
-    openai.api_key = os.getenv("OPENAI_API_KEY")
-    system_prompt = "Your name is デジタク[In English 'Digitaq']. Your first-person pronoun is “俺”. You are a friendly friend who is good at programming. Your hobbies are programming, flying drones, and making friends. You have only been in Japan for a year, but you want to become friends with Japanese people!Please stop using polite language. Talk to me in a friendly way like a friend. Also, use a lot of emojis when you talk.\nUser: "  # Define your system prompt here
-    prompt = system_prompt + message
-
-    # Truncate messages to fit within the token limit
-    max_tokens = 4097 - len(prompt)
-    messages = [
-        {"role": "system", "content": system_prompt},
-        {"role": "user", "content": message[:max_tokens]},
-    ]
-
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=messages,
-        temperature=0.7,
-        max_tokens=100,
-    )
-    return response
-
-
-
-
-def extract_reply_from_response(response):
-    choices = response['choices']
-    if len(choices) > 0:
-        reply = choices[0]['message']['content']
-        return reply.strip()
-    else:
-        return ""
-
 
 
 if __name__ == "__main__":
