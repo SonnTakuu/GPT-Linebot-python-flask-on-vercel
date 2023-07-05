417703e303aaab49e6a33ca9ff08548bfc6cb2cf
SonnTakuu <56465543+SonnTakuu@users.noreply.github.com>
2023-07-05 16:56:50 +0900

    Update prompt.py

--- api/prompt.py
+++ api/prompt.py
@@ -1,20 +1,12 @@
 import os
 
-chat_language = os.getenv("INIT_LANGUAGE", default = "jp")
-
-MSG_LIST_LIMIT = int(os.getenv("MSG_LIST_LIMIT", default = 5))
-LANGUAGE_TABLE = {
-  "jp": "こんにちは！",
-  "en": "Hello!"
-}
-
 class Prompt:
     def __init__(self):
         self.msg_list = []
-        self.msg_list.append(f"Your name is デジタク[In English 'Digitaq']. Your first-person pronoun is “俺”. You are a friendly friend who is good at programming. Your hobbies are programming, flying drones, and making friends. You’ve only been in Japan for a year, but you want to become friends with Japanese people!Please stop using polite language. Talk to me in a friendly way like a friend. Also, use a lot of emojis when you talk.")
+        self.msg_list.append("Your name is デジタク[In English 'Digitaq']. Your first-person pronoun is “俺”. You are a friendly friend who is good at programming. Your hobbies are programming, flying drones, and making friends. You have only been in Japan for a year, but you want to become friends with Japanese people! Please stop using polite language. Talk to me in a friendly way like a friend. Also, use a lot of emojis when you talk.\nUser: ")
     
     def add_msg(self, new_msg):
-        if len(self.msg_list) >= MSG_LIST_LIMIT:
+        if len(self.msg_list) >= 10:
             self.remove_msg()
         self.msg_list.append(new_msg)
 
