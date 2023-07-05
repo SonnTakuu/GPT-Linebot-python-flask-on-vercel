8cf1951689f0b8310d9f25818a429f0c74b6c133
SonnTakuu <56465543+SonnTakuu@users.noreply.github.com>
2023-07-05 20:41:04 +0900

    Update prompt.py我们需要在添加新消息之前检查上下文令牌数量是否超过了模型的最大上下文长度，并在必要时移除旧消息。这样我们可以确保在每次调用 get_response 方法时，上下文令牌数量不会超过模型的限制。

--- api/prompt.py
+++ api/prompt.py
@@ -12,24 +12,23 @@ LANGUAGE_TABLE = {
 class Prompt:
     def __init__(self):
         self.msg_list = []
-        self.max_tokens = int(os.getenv("OPENAI_MAX_TOKENS", default=240))
-        self.current_tokens = 0
+        self.max_context_tokens = 4097
         self.msg_list.append(
             "Your name is デジタク[In English 'Digitaq']. Your first-person pronoun is “俺”. You are a friendly friend who is good at programming. Your hobbies are programming, flying drones, and making friends. You have only been in Japan for a year, but you want to become friends with Japanese people! Please stop using polite language. Talk to me in a friendly way like a friend. Also, use a lot of emojis when you talk.\nUser: ")
 
 
     def add_msg(self, new_msg):
         tokens = len(new_msg.split())
-        while self.current_tokens + tokens > self.max_tokens:
-            removed_msg = self.msg_list.pop(0)
-            self.current_tokens -= len(removed_msg.split())
+        while self.get_total_tokens() + tokens > self.max_context_tokens:
+            self.remove_oldest_msg()
         self.msg_list.append(new_msg)
-        self.current_tokens += tokens
+
+    def remove_oldest_msg(self):
+        if self.msg_list:
+            self.msg_list.pop(0)
+
+    def get_total_tokens(self):
+        return sum(len(msg.split()) for msg in self.msg_list)
 
     def generate_prompt(self):
-        return '\n'.join(self.msg_list[-MSG_LIST_LIMIT:])
-
-    def remove_msg(self):
-        removed_msg = self.msg_list.pop(0)
-        self.current_tokens -= len(removed_msg.split())
-
+        return '\n'.join(self.msg_list)
