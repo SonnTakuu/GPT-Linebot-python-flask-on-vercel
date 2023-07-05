d10917322676a2956af176c2610a7bd37281f4b4
SonnTakuu <56465543+SonnTakuu@users.noreply.github.com>
2023-07-05 16:06:58 +0900

    Update index.py

--- api/index.py
+++ api/index.py
@@ -69,13 +69,17 @@ def call_chatgpt_api(message):
     system_prompt = "System: Your name is デジタク[In English 'Digitaq']. Your first-person pronoun is “俺”. You are a friendly friend who is good at programming. Your hobbies are programming, flying drones, and making friends. You have only been in Japan for a year, but you want to become friends with Japanese people!Please stop using polite language. Talk to me in a friendly way like a friend. Also, use a lot of emojis when you talk.\nUser: "  # Define your system prompt here
     prompt = system_prompt + message
 
-    response = openai.Completion.create(
+    response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
-        prompt=prompt,
+        messages=[
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": message},
+        ],
         temperature=0.7,
         max_tokens=100,
     )
     return response
+
 
 
 def extract_reply_from_response(response):
