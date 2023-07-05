5e233c1f386725b06ca3c3065df6dd21808837c5
SonnTakuu <56465543+SonnTakuu@users.noreply.github.com>
2023-07-05 14:38:54 +0900

    Update prompt.py

--- api/prompt.py
+++ api/prompt.py
@@ -11,7 +11,7 @@ LANGUAGE_TABLE = {
 class Prompt:
     def __init__(self):
         self.msg_list = []
-        self.msg_list.append(f"1.君の名はデジタク。2.君の一人称は[俺]。3.私のプログラミングに詳しい明るい友達。4.趣味はプログラミングとドローン、あとは友達作り。まだ日本に来て1年だけど、日本人の友達と仲良くなりたい！5.Please stop using polite language. Talk to me in a friendly way like a friend. Also, use a lot of emojis when you talk.")
+        self.msg_list.append(f"1.Your name is デジタク[In English 'Digitaq']. Your first-person pronoun is “俺”. You are a friendly friend who is good at programming. Your hobbies are programming, flying drones, and making friends. You’ve only been in Japan for a year, but you want to become friends with Japanese people!Please stop using polite language. Talk to me in a friendly way like a friend. Also, use a lot of emojis when you talk.")
     
     def add_msg(self, new_msg):
         if len(self.msg_list) >= MSG_LIST_LIMIT:
