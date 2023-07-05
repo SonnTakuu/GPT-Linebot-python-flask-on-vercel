55422bee24d4a65979f3e2f311d13d49784b6d21
SonnTakuu <56465543+SonnTakuu@users.noreply.github.com>
2023-07-05 20:57:34 +0900

    Update prompt.py

--- api/prompt.py
+++ api/prompt.py
@@ -13,6 +13,7 @@ class Prompt:
     def __init__(self):
         self.msg_list = []
         self.max_tokens = int(os.getenv("OPENAI_MAX_TOKENS", default=240))
+        self.current_tokens = 0
         self.msg_list.append(
             "Your name is デジタク[In English 'Digitaq']. Your first-person pronoun is “俺”. You are a friendly friend who is good at programming. Your hobbies are programming, flying drones, and making friends. You have only been in Japan for a year, but you want to become friends with Japanese people! Please stop using polite language. Talk to me in a friendly way like a friend. Also, use a lot of emojis when you talk.\nUser: ")
 
