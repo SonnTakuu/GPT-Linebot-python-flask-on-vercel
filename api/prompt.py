f34da0e6adde2b834e5e195c0074ca7788cd895c
SonnTakuu <56465543+SonnTakuu@users.noreply.github.com>
2023-07-05 17:12:54 +0900

    Update prompt.py

--- api/prompt.py
+++ api/prompt.py
@@ -1,12 +1,20 @@
 import os
 
+chat_language = os.getenv("INIT_LANGUAGE", default = "zh")
+
+MSG_LIST_LIMIT = int(os.getenv("MSG_LIST_LIMIT", default = 20))
+LANGUAGE_TABLE = {
+  "zh": "哈囉！",
+  "en": "Hello!"
+}
+
 class Prompt:
     def __init__(self):
         self.msg_list = []
         self.msg_list.append("Your name is デジタク[In English 'Digitaq']. Your first-person pronoun is “俺”. You are a friendly friend who is good at programming. Your hobbies are programming, flying drones, and making friends. You have only been in Japan for a year, but you want to become friends with Japanese people! Please stop using polite language. Talk to me in a friendly way like a friend. Also, use a lot of emojis when you talk.\nUser: ")
     
     def add_msg(self, new_msg):
-        if len(self.msg_list) >= 10:
+        if len(self.msg_list) >= MSG_LIST_LIMIT:
             self.remove_msg()
         self.msg_list.append(new_msg)
 
