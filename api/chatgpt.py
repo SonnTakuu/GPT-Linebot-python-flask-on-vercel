d301f8706849d29fbcf38bbf58b5d3625d714b2d
SonnTakuu <56465543+SonnTakuu@users.noreply.github.com>
2023-07-05 20:49:17 +0900

    Update chatgpt.py

--- api/chatgpt.py
+++ api/chatgpt.py
@@ -5,18 +5,17 @@ import openai
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-
 class ChatGPT:
     def __init__(self):
         self.prompt = Prompt()
-        self.model = os.getenv("OPENAI_MODEL", default = "text-davinci-003")
-        self.temperature = float(os.getenv("OPENAI_TEMPERATURE", default = 0))
-        self.frequency_penalty = float(os.getenv("OPENAI_FREQUENCY_PENALTY", default = 0))
-        self.presence_penalty = float(os.getenv("OPENAI_PRESENCE_PENALTY", default = 0.6))
-        self.max_tokens = int(os.getenv("OPENAI_MAX_TOKENS", default = 240))
+        self.model = os.getenv("OPENAI_MODEL", default="text-davinci-003")
+        self.temperature = float(os.getenv("OPENAI_TEMPERATURE", default=0))
+        self.frequency_penalty = float(os.getenv("OPENAI_FREQUENCY_PENALTY", default=0))
+        self.presence_penalty = float(os.getenv("OPENAI_PRESENCE_PENALTY", default=0.6))
+        self.max_tokens = int(os.getenv("OPENAI_MAX_TOKENS", default=240))
 
     def get_response(self):
-        try:
+        while True:
             response = openai.Completion.create(
                 model=self.model,
                 prompt=self.prompt.generate_prompt(),
@@ -25,11 +24,17 @@ class ChatGPT:
                 presence_penalty=self.presence_penalty,
                 max_tokens=self.max_tokens
             )
-            return response['choices'][0]['text'].strip()
-        except openai.error.APIError as e:
-            # 发生错误时移除最旧的消息
-            self.prompt.remove_msg()
-            return self.get_response()  # 重新请求生成回复
+            choices = response['choices']
+            if len(choices) > 0:
+                reply_text = choices[0]['text']
+                tokens_used = response['usage']['total_tokens']
+                if tokens_used <= self.max_tokens:
+                    self.prompt.add_msg(reply_text)
+                    return reply_text
+                else:
+                    self.prompt.remove_msg()
+            else:
+                return ''
 
     def add_msg(self, text):
         self.prompt.add_msg(text)
