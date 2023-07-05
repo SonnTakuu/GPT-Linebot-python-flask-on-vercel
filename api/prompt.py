5293f1c676a8e2ef9902fe5af8005cbe3153825c
SonnTakuu <56465543+SonnTakuu@users.noreply.github.com>
2023-07-05 21:00:51 +0900

    Update prompt.py

--- api/prompt.py
+++ api/prompt.py
@@ -13,24 +13,16 @@ class Prompt:
     def __init__(self):
         self.msg_list = []
         self.max_tokens = int(os.getenv("OPENAI_MAX_TOKENS", default=240))
-        self.current_tokens = 0
         self.msg_list.append(
             "Your name is デジタク[In English 'Digitaq']. Your first-person pronoun is “俺”. You are a friendly friend who is good at programming. Your hobbies are programming, flying drones, and making friends. You have only been in Japan for a year, but you want to become friends with Japanese people! Please stop using polite language. Talk to me in a friendly way like a friend. Also, use a lot of emojis when you talk.\nUser: ")
 
     def add_msg(self, new_msg):
-        tokens = len(new_msg.split())
-        while self.current_tokens + tokens > self.max_tokens:
-            removed_msg = self.msg_list.pop(0)
-            self.current_tokens -= len(removed_msg.split())
         self.msg_list.append(new_msg)
-        self.current_tokens += tokens
+        self.adjust_prompt_length()
 
-
-    def adjust_messages(self, tokens_to_remove):
-        while tokens_to_remove > 0:
-            removed_msg = self.msg_list.pop(0)
-            tokens_removed = len(removed_msg.split())
-            tokens_to_remove -= tokens_removed
+    def adjust_prompt_length(self):
+        while self.get_total_tokens() > self.max_tokens:
+            self.msg_list.pop(0)
 
     def get_total_tokens(self):
         return sum(len(msg.split()) for msg in self.msg_list)
